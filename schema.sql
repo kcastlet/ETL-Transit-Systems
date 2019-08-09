a3a89b6d0ba4c7c1608334ff615bc83838055603
Rob Fondaw <robsmac@Robs-MacBook-Air.local>
2019-08-09 12:27:46 -0500

    Updated Schema

--- schema.sql
+++ schema.sql
@@ -1,30 +1,30 @@
 
 
 CREATE TABLE cities (
-"City_ID" INT PRIMARY KEY NOT NULL,
-"City_Name" VARCHAR NOT NULL,
-"Start_Year" VARCHAR NOT NULL,
-"Country" VARCHAR NOT NULL
+City_ID INT PRIMARY KEY NOT NULL,
+City_Name VARCHAR NOT NULL,
+Start_Year VARCHAR NOT NULL,
+Country VARCHAR NOT NULL
 );
 
 CREATE TABLE stations (
-"Station_ID" INT PRIMARY KEY NOT NULL,
-"Station_Name" VARCHAR ,
-"Buildstart" VARCHAR,
-"Opening" VARCHAR,
-"City_ID" INT NOT NULL
+Station_ID INT PRIMARY KEY NOT NULL,
+Station_Name VARCHAR,
+Buildstart VARCHAR,
+Opening VARCHAR,
+City_ID INT NOT NULL
 );
 
 
 CREATE TABLE master (
-"Station_ID" INT PRIMARY KEY NOT NULL,
-"City_ID" INT NOT NULL,
-"City_Name" VARCHAR,
-"Start_Year" VARCHAR,
-"Country" VARCHAR,
-"Station_Name" VARCHAR,
-"Buildstart" VARCHAR,
-"Opening" VARCHAR
+Station_ID INT PRIMARY KEY NOT NULL,
+City_ID INT NOT NULL,
+City_Name VARCHAR,
+Start_Year VARCHAR,
+Country VARCHAR,
+Station_Name VARCHAR,
+Buildstart VARCHAR,
+Opening VARCHAR
 );
 
 
